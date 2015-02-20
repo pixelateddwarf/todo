834dc2a543403047602e9eaa3ace95b5e42734f8
pixelateddwarf <pixelateddwarf@gmail.com>
2015-02-20 16:57:31 -0500

    Made changes and added delete to items

--- spec/views/todo_lists/show.html.erb_spec.rb
+++ spec/views/todo_lists/show.html.erb_spec.rb
@@ -10,7 +10,6 @@ describe "todo_lists/show" do
 
   it "renders attributes in <p>" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
     rendered.should match(/MyText/)
   end
