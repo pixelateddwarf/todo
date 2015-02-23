d7e1c0f34ae869e6294b51ef7de049a18421f678
pixelateddwarf <pixelateddwarf@gmail.com>
2015-02-23 12:03:18 -0500

    Styling and fixing problems

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,9 +1,8 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
-  
+
   validates :content, presence: true,
                       length: { minimum: 2 }
-end
 
   scope :complete, -> { where("completed_at is not null") }
   scope :incomplete, -> { where(completed_at: nil) }
